0ccbba08610fba1799bcdfc6cd0232a134732684
Ivan Misarosh <ivan.misarosh05@gmail.com>
2024-06-16 19:20:18 +0300

    turret and hull assets + turret points to cursor

--- tank.py
+++ tank.py
@@ -10,14 +10,17 @@ class Tank:
         self.turret_angle = 0
         self.speed = 300
 
-        self.original_hull = pygame.Surface((50, 50), pygame.SRCALPHA)
-        self.original_turret = pygame.Surface((10, 50), pygame.SRCALPHA)
-        self.original_hull.fill((255, 0, 0))  # Red rectangle
-        self.original_turret.fill((0, 255, 0))  # Green rectangle
+        self.original_hull = pygame.image.load("assets/hull2.png").convert_alpha()
+        self.original_hull = pygame.transform.rotate(self.original_hull, 180)
+        width, height = self.original_hull.get_size()
+        self.original_hull = pygame.transform.scale(self.original_hull, (width * 0.15, height * 0.15))
+
+        self.original_turret = pygame.image.load("assets/turret.png").convert_alpha()
+        width, height = self.original_turret.get_size()
+        self.original_turret = pygame.transform.scale(self.original_turret, (width * 0.40, height * 0.40))
 
         self.hull = self.original_hull
         self.turret = self.original_turret
-
 
     def update(self, dt):
         keys = pygame.key.get_pressed()
@@ -25,21 +28,28 @@ class Tank:
             self.pos.y -= self.speed * dt * math.cos(math.radians(self.hull_angle))
             self.pos.x -= self.speed * dt * math.sin(math.radians(self.hull_angle))
         if keys[pygame.K_s]:
-            self.pos.y += self.speed * dt
+            self.pos.y += self.speed * dt * math.cos(math.radians(self.hull_angle))
+            self.pos.x += self.speed * dt * math.sin(math.radians(self.hull_angle))
         if keys[pygame.K_a]:
             self.hull_angle += 100 * dt
         if keys[pygame.K_d]:
             self.hull_angle += -100 * dt
-        if pygame.mouse.get_rel()[0] > 0 or pygame.mouse.get_rel()[1] > 0:
-            mouse_pos = pygame.mouse.get_pos()
+
+        mouse_pos = pygame.mouse.get_pos()
+        self.turret_angle = math.degrees(math.atan2(-(mouse_pos[1] - self.pos.y), mouse_pos[0] - self.pos.x))
 
     def draw(self):
-        player_image = pygame.Surface((100, 100), pygame.SRCALPHA)
-        player_image.blit(self.hull, (5, 20))  # Position rect1 within the player image
-        player_image.blit(self.turret, (25, 0))  # Position rect2 within the player image
+        # Rotate the hull
+        rotated_hull = pygame.transform.rotate(self.original_hull, self.hull_angle)
+        rotated_hull_rect = rotated_hull.get_rect(center=self.pos)
 
-        player_image = pygame.transform.rotate(player_image, self.hull_angle)
-        player_rect = player_image.get_rect(center=self.pos)
+        # Rotate the turret
+        rotated_turret = pygame.transform.rotate(self.original_turret, self.turret_angle - 90)
+        rotated_turret_rect = rotated_turret.get_rect(center=self.pos)
 
-        self.screen.blit(player_image, player_rect)
+        # Blit the rotated hull and turret to the screen
+        self.screen.blit(rotated_hull, rotated_hull_rect)
+        self.screen.blit(rotated_turret, rotated_turret_rect)
+
+
 
