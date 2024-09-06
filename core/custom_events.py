5ac56b843149ffaf8c6cee56fc1ed122f0c523bb
Ivan Misarosh <ivan.misarosh05@gmail.com>
2024-09-06 22:01:47 +0300

    custom event setup

---
+++ core/custom_events.py
@@ -0,0 +1,7 @@
+import pygame
+
+# Custom event type
+START_SINGLE_PLAYER_GAME = pygame.event.custom_type()
+START_MULTI_PLAYER_GAME = pygame.event.custom_type()
+OPEN_SETTINGS_MENU = pygame.event.custom_type()
+
